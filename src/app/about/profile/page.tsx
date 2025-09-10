42b9fed8fd27bf53116ea7ebac7f931ee22d54db
egychandra <egy.chandra.l@gmail.com>
2025-09-10 20:35:26 +0700

    setup nextjs app router

---
+++ src/app/about/profile/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./navbar"
+
+const ProfilePage = () => {
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <Navbar />
+    </div>
+  )
+}
+
+export default ProfilePage
