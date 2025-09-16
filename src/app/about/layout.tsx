29b58ed952c0d327427f66eec4f7d9a5d2a8cf91
egychandra <egy.chandra.l@gmail.com>
2025-09-16 22:35:08 +0700

    link & navigation

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,8 +1,8 @@
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <aside className="fixed right-0 top-12 z-10 h-screen w-60 bg-gray-800">
-        <ul className="text-blue-300 p-5">
+      <aside className="fixed right-0 top-12 z-[-1] h-screen w-60 bg-gray-800">
+        <ul className="text-blue-300 px-5 py-10">
           <li>Home</li>
           <li>About</li>
           <li>Profile</li>
